862f043b579f5927d55081b2902becf9588f873e
Geoff Kelly <geoffkellync@gmail.com>
2023-10-31 17:44:24 -0400

    fixed bug to user stripe portal

--- src/api/userPortal.ts
+++ src/api/userPortal.ts
@@ -5,7 +5,11 @@ const url = import.meta.env.VITE_REACT_APP_API_ENDPOINT;
 
 const createPortalSession = async (): Promise<void> => {
     try {
-       await axiosWithAuth().post(`${url}/stripe/create-portal-session`);
+       const res = await axiosWithAuth().post(`${url}/stripe/create-portal-session`);
+
+       const redirectUrl = res.data.data;
+
+       window.location.href = redirectUrl;
 
     } catch (error) {
         console.log(error);
